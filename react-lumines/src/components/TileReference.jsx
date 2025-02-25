0d0fdc5a09829978aa6dea95ef7d9a8e833f87fe
Douglas Storz <douglasstorz@gmail.com>
2025-02-25 01:55:58 -0500

    Full set of reference tiles

--- react-lumines/src/components/TileReference.jsx
+++ react-lumines/src/components/TileReference.jsx
@@ -4,7 +4,12 @@ export default function TileReference() {
   return (
     <header>
       <section className="tile-reference">
+        <Tile arrangement="wwww" />
+        <Tile arrangement="cccc" />
         <Tile arrangement="wwwc" />
+        <Tile arrangement="cccw" />
+        <Tile arrangement="wwcc" />
+        <Tile arrangement="cwwc" />
       </section>
     </header>
   );
